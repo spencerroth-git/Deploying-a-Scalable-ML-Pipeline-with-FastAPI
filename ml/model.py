bbf4039eaa7211687422aab624b072780bec5ced
Spencer <spencer1roth@icloud.com>
2026-03-09 13:22:34 -0600

    Added code under relevant "TODO" comments and updated the model card with information about the model.

--- ml/model.py
+++ ml/model.py
@@ -2,6 +2,7 @@ import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
 # TODO: add necessary import
+from sklearn.ensemble import RandomForestClassifier
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -20,6 +21,9 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
     # TODO: implement the function
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
+    return model
     pass
 
 
@@ -60,6 +64,8 @@ def inference(model, X):
         Predictions from the model.
     """
     # TODO: implement the function
+    preds = model.predict(X)
+    return preds
     pass
 
 def save_model(model, path):
@@ -73,11 +79,16 @@ def save_model(model, path):
         Path to save pickle file.
     """
     # TODO: implement the function
+    with open(path, 'wb') as f:
+        pickle.dump(model, f)
     pass
 
 def load_model(path):
     """ Loads pickle file from `path` and returns it."""
     # TODO: implement the function
+    with open(path, 'rb') as f:
+        model = pickle.load(f)
+    return model
     pass
 
 
@@ -118,11 +129,15 @@ def performance_on_categorical_slice(
 
     """
     # TODO: implement the function
+
+    slice_data = data[data[column_name] == slice_value]
+
     X_slice, y_slice, _, _ = process_data(
         # your code here
         # for input data, use data in column given as "column_name", with the slice_value 
         # use training = False
+        slice_data, categorical_features=categorical_features, label=label, encoder=encoder, lb=lb, training=False    
     )
-    preds = None # your code here to get prediction on X_slice using the inference function
+    preds = inference(model, X_slice) # your code here to get prediction on X_slice using the inference function
     precision, recall, fbeta = compute_model_metrics(y_slice, preds)
     return precision, recall, fbeta
--- model_card_template.md
+++ model_card_template.md
@@ -3,16 +3,30 @@
 For additional information see the Model Card paper: https://arxiv.org/pdf/1810.03993.pdf
 
 ## Model Details
+This model is a supervised learning classification model that is trained to predict whether an individual's annual income exceeds $50,000 based on demographic and employment related features.
 
 ## Intended Use
+The intended use of this model is for educational and exploratory purposes.
 
 ## Training Data
+This model was trained using the US Census Income dataset, which contains the demographic and employment information necessary to train the model.
 
 ## Evaluation Data
+This model was trained by separating a test dataset from the original dataset using a method known as a train-test split. Performance metrics were then gathered on the test dataset to determine how well the model operates independent of the entirety of the rest of the data.
 
 ## Metrics
 _Please include the metrics used and your model's performance on those metrics._
+There were three metrics used to assess model performance:
+-Precision
+-Recall
+-F1 Score
 
+Below are the results of the test dataset according to those metrics:
+Precision: .7485
+Recall: .6484
+F1 Score: .6949
 ## Ethical Considerations
+The data from the Census Income dataset may inadvertently introduce bias into the model. With this in mind, evaluation should be done to evaluate the presence of any bias, and mitigate those factors if found.
 
 ## Caveats and Recommendations
+One potential improvement would be using hyperparamter tuning and comparing additional models to the metrics found by using our selected model (RandomForestClassifier).
